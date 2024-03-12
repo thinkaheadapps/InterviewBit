3ffce2f977d3252e4a8fef593eac47e57fd234e3
Divya Rai <thinkaheadapps@gmail.com>
2024-03-12 14:11:40 +0530

    Comple Path in HelloWorld file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,14 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        
+
     }
 }
+
+/*
+/Users/divyarai/Desktop/CompleteJava/InterviewBit
+git add .
+git remote add origin https://github.com/thinkaheadapps/InterviewBit.git
+git push -u origin main
+git commit -m "first commit" 
+ */
